f014955aee16bff2beb6246d4742640e2e38de7b
wadekarlalit <lalitwadekr@gmail.com>
2026-05-24 11:41:25 +0530

    Added production backend url

--- client/src/hooks/useSocket.ts
+++ client/src/hooks/useSocket.ts
@@ -1,3 +1,4 @@
 import io from "socket.io-client";
 
-export const socket = io("http://localhost:5000")
+// export const socket = io("http://localhost:5000")
+export const socket = io("https://tic-tac-toe-y5xh.onrender.com")
